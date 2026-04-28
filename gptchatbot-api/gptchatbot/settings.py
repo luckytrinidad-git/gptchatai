527a4daea7a21a193c223773cea72844e62f5471
luckytrinidad-git <lucky3nidadtest123@gmail.com>
2026-04-29 02:17:48 +0800

    Added API in allowed hosts

--- gptchatbot-api/gptchatbot/settings.py
+++ gptchatbot-api/gptchatbot/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+!6+nguc4((e&d-hz%er3r2mha=t=a8*9wd%aja=8#m4l7!^@%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','13.213.49.77','13.215.160.167', 'localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','13.213.49.77','13.215.160.167', 'localhost','birgptchatbot-api']
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
